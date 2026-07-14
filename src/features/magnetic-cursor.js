61c6f7515c4e1a5a4729aa6caf22245e58db687b
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2026-07-14 13:24:48 +0200

    fix(magnetic-cursor): make cursor less laggy

--- src/features/magnetic-cursor.js
+++ src/features/magnetic-cursor.js
@@ -83,8 +83,8 @@ function ensureCursorSetup() {
 
   gsap.set(cursorEl, { xPercent: -150, yPercent: -125 });
 
-  const xTo = gsap.quickTo(cursorEl, 'x', { duration: 0.6, ease: 'power4' });
-  const yTo = gsap.quickTo(cursorEl, 'y', { duration: 0.6, ease: 'power4' });
+  const xTo = gsap.quickTo(cursorEl, 'x', { duration: 0.1, ease: 'power4' });
+  const yTo = gsap.quickTo(cursorEl, 'y', { duration: 0.1, ease: 'power4' });
 
   window.addEventListener('mousemove', (e) => {
     xTo(e.clientX);
